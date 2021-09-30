269a14fb3356a881a7b1d694c85d090638068308
miya-start <0000000to1@gmail.com>
2021-09-30 19:48:00 +0900

    Code formatting with prettier

--- public/script.js
+++ public/script.js
@@ -1,26 +1,26 @@
-const socket = io(location.host);
-const msgForm = document.querySelector("#send-container");
-const msgInput = document.querySelector("#message-input");
-const msgContainer = document.querySelector("#message-container");
-const roomContainer = document.querySelector("#room-container");
+const socket = io(location.host)
+const msgForm = document.querySelector('#send-container')
+const msgInput = document.querySelector('#message-input')
+const msgContainer = document.querySelector('#message-container')
+const roomContainer = document.querySelector('#room-container')
 
 if (msgForm != null) {
-  const name = prompt(`名前を入力してください`);
-  AppendMessage(`あなたは ${name} として参加しました。`);
-  socket.emit("new-user", roomName, name);
+  const name = prompt(`名前を入力してください`)
+  AppendMessage(`あなたは ${name} として参加しました。`)
+  socket.emit('new-user', roomName, name)
 
-  socket.on("chat-message", (data) => {
-    console.log(data);
-    AppendMessage(`${data.name}: ${data.message}`);
-  });
+  socket.on('chat-message', (data) => {
+    console.log(data)
+    AppendMessage(`${data.name}: ${data.message}`)
+  })
 
-  msgForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const message = msgInput.value;
-    var msg = AppendMessage(`あなた: ${message}`);
-    socket.emit("send-chat-message", roomName, message);
-    msgInput.value = "";
-  });
+  msgForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const message = msgInput.value
+    AppendMessage(`あなた: ${message}`)
+    socket.emit('send-chat-message', roomName, message)
+    msgInput.value = ''
+  })
 }
 
 /*
@@ -32,39 +32,39 @@ if (msgForm != null) {
       </div>
 */
 
-socket.on("room-created", (room) => {
-  const roomDiv = document.createElement("div");
-  roomDiv.innerHTML = ` 
+socket.on('room-created', (room) => {
+  const roomDiv = document.createElement('div')
+  roomDiv.innerHTML = `
   <div class="room-item">
   <p class="room-label">${room}</p>
   <a class="btn" href="/${room} "
     >参加する <span class="material-icons">chevron_right</span></a
   >
   </div>
-  `;
-  roomContainer.append(roomDiv);
-});
+  `
+  roomContainer.append(roomDiv)
+})
 
-socket.on("user-connected", (data) => {
-  console.log("New User: " + data);
-  AppendMessage(`${data} が参加しました`);
-  CreateToast(`${data} が参加しました`);
-});
+socket.on('user-connected', (data) => {
+  console.log('New User: ' + data)
+  AppendMessage(`${data} が参加しました`)
+  CreateToast(`${data} が参加しました`)
+})
 
-socket.on("user-disconnected", (data) => {
-  console.log(data + " が退出しました");
-  AppendMessageFromSender(`${data} が退出しました`);
-});
+socket.on('user-disconnected', (data) => {
+  console.log(data + ' が退出しました')
+  AppendMessageFromSender(`${data} が退出しました`)
+})
 
 function AppendMessage(message) {
-  const msgElem = document.createElement("div");
-  msgElem.innerText = message;
-  msgContainer.append(msgElem);
+  const msgElem = document.createElement('div')
+  msgElem.innerText = message
+  msgContainer.append(msgElem)
 }
 
 function AppendMessageFromSender(message) {
-  const msgElem = document.createElement("div");
-  msgElem.classList.add("message");
-  msgElem.innerText = message;
-  msgContainer.append(msgElem);
+  const msgElem = document.createElement('div')
+  msgElem.classList.add('message')
+  msgElem.innerText = message
+  msgContainer.append(msgElem)
 }
